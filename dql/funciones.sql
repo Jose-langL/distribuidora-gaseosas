9c641a0b3d5532349e7ce433c1d634d4a18203d7
Jose-langL <totherrerajoseluis@gmail.com>
2026-08-12 16:50:01 -0600

    feat: :sparkles: (dql) Crear funcion de total Iva para los  pedidos
    Creacion de calcular total iva para los pedidos

--- dql/funciones.sql
+++ dql/funciones.sql
@@ -21,3 +21,32 @@ BEGIN
     RETURN Sub_total;
 END// 
 DELIMITER ; 
+
+-- Calcular total con iva 
+
+DELIMITER // 
+CREATE FUNCTION fn_calcular_total_con_iva(p_id_pedido INT)
+RETURNS DECIMAL(5,2)
+DETERMINISTIC
+BEGIN 
+	DECLARE total_iva DECIMAL(5,2);
+	IF NOT EXISTS (
+		SELECT 1
+        FROM Detalles_pedidos
+        WHERE id_pedido = p_id_pedido 
+	) THEN 
+		SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'El pedido no existe';
+    END IF; 
+    
+    SELECT 
+		SUM(fn_calcular_sub_total(id_detalle) * 1.19 ) 
+        INTO total_iva
+		FROM Detalles_pedidos 
+        WHERE id_pedido = p_id_pedido;
+        
+	RETURN total_iva;
+END// 
+DELIMITER ; 
+
+SELECT fn_calcular_total_con_iva(1);
