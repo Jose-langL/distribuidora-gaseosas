7fee723b11a0e0774a6a70a6d1c690da8788b072
Jose-langL <totherrerajoseluis@gmail.com>
2026-08-12 20:19:14 -0600

    feat: :sparkles: (dql) Crear vista clientes activos
    Creacion de vista para saber los clientes activos

--- dql/vistas.sql
+++ dql/vistas.sql
@@ -17,3 +17,12 @@ CREATE VIEW vw_productos_bajo_stock AS
     FROM Productos P 
     WHERE P.stock_actual <= P.stock_minimo;
 SELECT * FROM vw_productos_bajo_stock;
+
+CREATE VIEW vw_clientes_activos AS
+	SELECT 
+		C.id AS id_cliente,
+		CONCAT(C.nombre, ' ', C.apellido) AS Cliente,
+        COUNT(P.codigo_pedido) AS Pedidos
+    FROM Pedidos P
+    INNER JOIN  Clientes C ON P.id_cliente = C.id
+    GROUP BY C.id, C.Nombre, C.apellido ;
